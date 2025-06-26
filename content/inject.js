f3f533c66341131897e09908b4eb210d9ece8b20
danielp67 <daniel.67100@hotmail.fr>
2025-06-26 23:50:03 +0200

    fix add maps and reddit buttons

--- content/inject.js
+++ content/inject.js
@@ -2,13 +2,11 @@
   const { activeModules = [] } = await chrome.storage.sync.get("activeModules");
 
   chrome.runtime.sendMessage({ action: "updateBadge", count: activeModules.length });
-      console.log("Chargement du module:");
 
   for (const module of activeModules) {
     try {
       const mod = await import(`../modules/${module}.js`);
-      console.log("Chargement du module:", module);
-      if (mod && typeof mod.run === 'function') {
+        if (mod && typeof mod.run === 'function') {
         mod.run();
       }
     } catch (e) {
